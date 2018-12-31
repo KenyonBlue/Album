5f177a73329999a625a420472cef7f776ee3d5fc
Kenyon Blue <bluekenyfleo@gmail.com>
2018-12-30 19:39:35 -0500

    adding header
    adding a header

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// Import library
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make Component
+
+const Header = () => {
+    return <Text>Albums!</Text>;
+}
+
+
+// make component available 
+export default Header;
