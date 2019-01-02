1990306be5fa6a94fe86fe0b96a9dbed00683ef1
Kenyon Blue <bluekenyfleo@gmail.com>
2019-01-01 20:21:41 -0500

    card section - debugger being weird reset

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return (
-        <View>
+        <Card>
+            <CardSection>
+            <View></View>
+
+
+            <View style={styles.headerContentStyle}>
             <Text>{props.album.title}</Text>
-        </View>
+            <Text>{props.album.artist}</Text>
+            </View>
+            </CardSection>
+        </Card>
     );
+};
+
+const styles {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
 };
 
 export default AlbumDetail;
